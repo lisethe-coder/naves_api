fb22abbda42e9687b58c11f0d0d8bcd6e18e3848
unknown <davgeek@outlook.com>
2015-07-28 20:01:33 -0700

    Parte 2

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 // Modelos
-var Nave = require('./app/models/nave');
+var Nave = require('./models/nave');
 
 // puerto del servidor
 // podra ser seteado como argumento en comando
@@ -24,10 +24,72 @@ var port = process.env.PORT || 3200;
 // RUTAS de la API
 var router = express.Router();
 
+// Middleware 
+router.use(function(req, res, next) {
+    console.log('Request a la API.');
+    next(); // llamamos a la función next para continuar
+});
+
 // ruta para probar nuestro servidor
 router.get('/', function(req, res){
 	res.json({message: 'Ándale, arriba arriba, yepa yepa'});
 });
+
+// Rutas basicas
+router.route('/naves')
+	.post(function(req, res){ // Ruta POST
+		var nave = new Nave();		
+		nave.nombre = req.body.nombre;
+		nave.categoria = req.body.categoria;
+		nave.motores = req.body.motores;
+		nave.peso = req.body.peso;
+		
+		nave.save(function(err){
+			if(err) res.send(err);
+			res.send({message:'Nave creada con exito'});
+		});
+	})
+	.get(function(req, res){ // Ruta GET
+		Nave.find(function(err, naves) {
+            if (err) res.send(err);
+            res.json(naves);
+        });
+	});
+
+// Rutas para Nave específica
+router.route('/naves/:id')
+    .get(function(req, res) { // GET por Id
+        Nave.findById(req.params.id, function(err, nave) {
+            if (err)res.send(err);
+            res.json(nave);
+        });
+    })
+	// actualizar Nave con Id
+    .put(function(req, res) {
+        Nave.findById(req.params.id, function(err, nave) {
+            if (err) res.send(err);
+
+            nave.nombre = req.body.nombre;
+			nave.categoria = req.body.categoria;
+			nave.motores = req.body.motores;
+			nave.peso = req.body.peso;
+
+            // Guardamos la Nave
+            nave.save(function(err) {
+                if (err) res.send(err);
+                res.json({message:'Nave actualizada!'});
+            });
+        });
+    })
+	// eliminar Nave con Id
+	.delete(function(req, res) {
+        Nave.remove({
+            _id: req.params.id
+        }, function(err, nave) {
+            if (err) res.send(err);
+            res.json({message:'Nave eliminada con exito'});
+        });
+    });
 
 // Registrar las rutas con prefijo /api
 app.use('/api', router);
